9bff8d7b85404fb13a6eb179e9e75bca966db03b
azure-sdk <azuresdk@microsoft.com>
2026-06-09 09:57:50 +0000

    Regenerate shard ba_co_1 (emitter 0.54.0-alpha.20260608.1)

--- sdk/cloudhealth/arm-cloudhealth/samples-dev/signalDefinitionsCreateOrUpdateSample.ts
+++ sdk/cloudhealth/arm-cloudhealth/samples-dev/signalDefinitionsCreateOrUpdateSample.ts
@@ -1,16 +1,15 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+import { CloudHealthClient } from "@azure/arm-cloudhealth";
+import { DefaultAzureCredential } from "@azure/identity";
+
 /**
  * This sample demonstrates how to create a SignalDefinition
  *
  * @summary create a SignalDefinition
- * x-ms-original-file: 2025-05-01-preview/SignalDefinitions_CreateOrUpdate.json
+ * x-ms-original-file: 2026-05-01-preview/SignalDefinitions_CreateOrUpdate.json
  */
-
-import { CloudHealthClient } from "@azure/arm-cloudhealth";
-import { DefaultAzureCredential } from "@azure/identity";
-
 async function signalDefinitionsCreateOrUpdate(): Promise<void> {
   const credential = new DefaultAzureCredential();
   const subscriptionId = "4980D7D5-4E07-47AD-AD34-E76C6BC9F061";
@@ -24,17 +23,16 @@ async function signalDefinitionsCreateOrUpdate(): Promise<void> {
         metricNamespace: "microsoft.compute/virtualMachines",
         metricName: "cpuusage",
         aggregationType: "None",
-        dimension: "nodename",
-        dimensionFilter: "node1",
+        dimensionFilter: "node eq '*'",
         displayName: "cpu usage",
         signalKind: "AzureResourceMetric",
         refreshInterval: "PT1M",
-        labels: { key4788: "ixfvzsfnpvkkbrce" },
+        tags: { key4788: "ixfvzsfnpvkkbrce" },
         timeGrain: "PT1M",
         dataUnit: "byte",
         evaluationRules: {
-          degradedRule: { operator: "LowerThan", threshold: "65" },
-          unhealthyRule: { operator: "LowerThan", threshold: "60" },
+          degradedRule: { operator: "GreaterThan", threshold: 70 },
+          unhealthyRule: { operator: "Dynamic", sensitivity: "Medium", lookBackWindow: "PT1H" },
         },
       },
     },
--- sdk/cloudhealth/arm-cloudhealth/src/api/entities/options.ts
+++ sdk/cloudhealth/arm-cloudhealth/src/api/entities/options.ts
@@ -4,16 +4,40 @@
 import { OperationOptions } from "@azure-rest/core-client";
 
 /** Optional parameters. */
+export interface EntitiesGetSignalRecommendationsOptionalParams extends OperationOptions {}
+
+/** Optional parameters. */
+export interface EntitiesGetDataAnnotationsOptionalParams extends OperationOptions {}
+
+/** Optional parameters. */
+export interface EntitiesAddDataAnnotationOptionalParams extends OperationOptions {}
+
+/** Optional parameters. */
+export interface EntitiesIngestHealthReportOptionalParams extends OperationOptions {}
+
+/** Optional parameters. */
+export interface EntitiesGetSignalHistoryOptionalParams extends OperationOptions {}
+
+/** Optional parameters. */
+export interface EntitiesGetHistoryOptionalParams extends OperationOptions {}
+
+/** Optional parameters. */
 export interface EntitiesListByHealthModelOptionalParams extends OperationOptions {
   /** Timestamp to use for the operation. When specified, the version of the resource at this point in time is retrieved. If not specified, the latest version is used. */
   timestamp?: Date;
 }
 
 /** Optional parameters. */
-export interface EntitiesDeleteOptionalParams extends OperationOptions {}
+export interface EntitiesDeleteOptionalParams extends OperationOptions {
+  /** Delay to wait until next poll, in milliseconds. */
+  updateIntervalInMs?: number;
+}
 
 /** Optional parameters. */
-export interface EntitiesCreateOrUpdateOptionalParams extends OperationOptions {}
+export interface EntitiesCreateOrUpdateOptionalParams extends OperationOptions {
+  /** Delay to wait until next poll, in milliseconds. */
+  updateIntervalInMs?: number;
+}
 
 /** Optional parameters. */
 export interface EntitiesGetOptionalParams extends OperationOptions {}
